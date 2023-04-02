ce435faa10288f977c850e765ece5a8bcc48eb28
musharaf Faqi <maamaanka55@gmail.com>
2023-04-02 14:47:27 +0300

    Success

--- sample_crud/app/urls.py
+++ sample_crud/app/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from django.contrib import admin
 from app import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('update/<id>/', views.update, name='update'),
+#     path('<int:id>/delete/', views.delete, name='delete'),
+#     # path('add_class', views.add_class, name='add_class'),
+# ]
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('update/<id>/', views.update, name='update'),
-    path('<int:id>/delete/', views.delete, name='delete'),
-    # path('add_class', views.add_class, name='add_class'),
+    path('create/',views.create, name='create'),
+    path('read/',views.read, name='read'),
+    path('edit/<id>',views.edit, name='edit'),
+    path('edit/update/<id>',views.update, name='update'),
+    path('delete/<id>',views.delete, name='delete'),
 ]
