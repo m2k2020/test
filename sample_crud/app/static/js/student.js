9a077d5b92aafbd852e475d8c50af1ca84829378
musharaf Faqi <maamaanka55@gmail.com>
2023-04-11 12:41:27 +0300

    Success

--- sample_crud/app/static/js/student.js
+++ sample_crud/app/static/js/student.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     $.getJSON('fetch_data/',function(data){
-        $('#myDropdown').append("<option >Select</option>");
+        $('#myDropdown').append("<option value=''>Select</option>");
         $.each(data,function(key,value){
             $('#myDropdown').append("<option value='" + value.id + "'>" + value.name + "</option>");
         })
@@ -12,8 +12,18 @@ $(document).ready(function(){
         $age = $("#stage").val();
         $grade_id = $("#myDropdown").val();
 
-        if ($name == "" && $age == "" && $grade_id == ""){
-            alert("Please select The Balnk space ");
+        if ($name !== '' && $age !== '' && $grade_id !== ''){
+        //    $('#nameError').text("Please select a name for this student");
+            alert("Success!");
+            document.getElementById("myForm").reset();
+            $('#nameError').text("");
+            $('#ageError').text("");
+            $('#gradeError').text("")
+        }
+        else{
+            $('#nameError').text("Please select a name for this student");
+            $('#ageError').text("Please select a age for this student");
+            $('#gradeError').text("Please select a grade for this student");
         }
    }) 
 
