8213c85202f125bb3a37de69db6caa685b73c780
musharaf Faqi <maamaanka55@gmail.com>
2023-04-11 13:39:31 +0300

    Success

--- sample_crud/app/static/js/student.js
+++ sample_crud/app/static/js/student.js
@@ -7,24 +7,53 @@ $(document).ready(function(){
         })
     })
 
-   $("#createStudent").on('click',function(){
+
+
+
+
+   $('#myForm').submit(function(e){
+        e.preventDefault();
         $name = $("#stname").val();
         $age = $("#stage").val();
         $grade_id = $("#myDropdown").val();
 
-        if ($name !== '' && $age !== '' && $grade_id !== ''){
-        //    $('#nameError').text("Please select a name for this student");
-            alert("Success!");
-            document.getElementById("myForm").reset();
-            $('#nameError').text("");
-            $('#ageError').text("");
-            $('#gradeError').text("")
-        }
-        else{
-            $('#nameError').text("Please select a name for this student");
-            $('#ageError').text("Please select a age for this student");
-            $('#gradeError').text("Please select a grade for this student");
-        }
+        $.ajax({
+            url: 'createStudent/',
+            type: 'POST',
+            data:{
+                'name':$name,
+                'age':$age,
+                'grade_id':$grade_id,
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+
+            },
+            success: function(data){
+                alert('Successfully Created Data');
+                console.log(data)
+
+                document.getElementById("myForm").reset()
+            },
+            error: function(data){
+                console.log('Error is Occured' + data);
+            }
+        })
+
+        // alert("Name is " + $name + " and age is " + $age+ " and grade_id is " + $grade_id);
+       
+
+        // if ($name !== '' && $age !== '' && $grade_id !== ''){
+        // //    $('#nameError').text("Please select a name for this student");
+        //     alert("Success!");
+        //     document.getElementById("myForm").reset();
+        //     $('#nameError').text("");
+        //     $('#ageError').text("");
+        //     $('#gradeError').text("")
+        // }
+        // else{
+        //     $('#nameError').text("Please select a name for this student");
+        //     $('#ageError').text("Please select a age for this student");
+        //     $('#gradeError').text("Please select a grade for this student");
+        // }
    }) 
 
 
