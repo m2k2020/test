9a7dff97c8635963ea1d0494d5a6d569ba4e8e3c
musharaf Faqi <maamaanka55@gmail.com>
2023-04-17 15:55:50 +0300

    Success

--- testingdb/app/views.py
+++ testingdb/app/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'accounts/login2.html')
+
+def register(request):
+    return render(request,'accounts/register.html')
+
+def forgot(request):
+    return render(request,'accounts/forgot.html')
+
+
+def staffs(request):
+    return render(request,'accounts/staffs.html')
+
+
+def house(request):     
+    return render(request,'Enviroment/house.html')
+
+
+def renter(request):
+
+    return render(request,'Enviroment/renter.html')
+
+def enviroment(request):
+
+    return render(request,'Enviroment/enviroment.html')
+
+
+
+
+def cleaning(request):
+    return render(request,'Enviroment/cleaning.html')
+
+def reports(request):
+    return render(request,'Enviroment/reports.html')
+
+def Payment_Method(request):
+    return render(request,'Enviroment/Payment_Method.html')
+
+
+def Transaction(request):
+    return render(request,'Enviroment/Transaction.html')
+
+
+def Reports2(request):
+    return render(request,'Enviroment/Reports2.html')
+
+
+
+
