667694eded845dfa138190da539c5264f0fd0893
musharaf Faqi <maamaanka55@gmail.com>
2023-04-20 11:32:40 +0300

    Success

--- testingdb/app/static/assets/js/page/widget-chart.js
+++ testingdb/app/static/assets/js/page/widget-chart.js
@@ -60,66 +60,66 @@ $(".sparkline-bar").sparkline(sparkline_values_bar, {
 });
 
 
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-    datasets: [{
-      label: 'Statistics',
-      data: [460, 458, 330, 502, 430, 610, 488],
-      borderWidth: 2,
-      backgroundColor: 'rgba(145,141,197,.8)',
-      borderWidth: 0,
-      borderColor: 'transparent',
-      pointBorderWidth: 0,
-      pointRadius: 3.5,
-      pointBackgroundColor: 'transparent',
-      pointHoverBackgroundColor: 'rgba(63,82,227,.8)',
-    }, {
-      label: 'Statistics',
-      data: [390, 600, 390, 280, 600, 430, 638],
-      borderWidth: 2,
-      backgroundColor: 'rgba(58,184,214,.7)',
-      borderWidth: 0,
-      borderColor: 'transparent',
-      pointBorderWidth: 0,
-      pointRadius: 3.5,
-      pointBackgroundColor: 'transparent',
-      pointHoverBackgroundColor: 'rgba(254,86,83,.8)',
-    }]
-  },
-  options: {
-    legend: {
-      display: false
-    },
-    scales: {
-      yAxes: [{
-        gridLines: {
-          drawBorder: false,
-          color: '#f2f2f2',
-        },
-        ticks: {
-          beginAtZero: true,
-          stepSize: 200,
-          fontColor: "#9aa0ac", // Font Color
-          callback: function (value, index, values) {
-            return value;
-          }
-        }
-      }],
-      xAxes: [{
-        gridLines: {
-          display: false,
-          tickMarkLength: 15,
-        },
-        ticks: {
-          fontColor: "#9aa0ac", // Font Color
-        }
-      }]
-    },
-  }
-});
+// var ctx = document.getElementById("myChart").getContext('2d');
+// var myChart = new Chart(ctx, {
+//   type: 'line',
+//   data: {
+//     labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+//     datasets: [{
+//       label: 'Statistics',
+//       data: [460, 458, 330, 502, 430, 610, 488],
+//       borderWidth: 2,
+//       backgroundColor: 'rgba(145,141,197,.8)',
+//       borderWidth: 0,
+//       borderColor: 'transparent',
+//       pointBorderWidth: 0,
+//       pointRadius: 3.5,
+//       pointBackgroundColor: 'transparent',
+//       pointHoverBackgroundColor: 'rgba(63,82,227,.8)',
+//     }, {
+//       label: 'Statistics',
+//       data: [390, 600, 390, 280, 600, 430, 638],
+//       borderWidth: 2,
+//       backgroundColor: 'rgba(58,184,214,.7)',
+//       borderWidth: 0,
+//       borderColor: 'transparent',
+//       pointBorderWidth: 0,
+//       pointRadius: 3.5,
+//       pointBackgroundColor: 'transparent',
+//       pointHoverBackgroundColor: 'rgba(254,86,83,.8)',
+//     }]
+//   },
+//   options: {
+//     legend: {
+//       display: false
+//     },
+//     scales: {
+//       yAxes: [{
+//         gridLines: {
+//           drawBorder: false,
+//           color: '#f2f2f2',
+//         },
+//         ticks: {
+//           beginAtZero: true,
+//           stepSize: 200,
+//           fontColor: "#9aa0ac", // Font Color
+//           callback: function (value, index, values) {
+//             return value;
+//           }
+//         }
+//       }],
+//       xAxes: [{
+//         gridLines: {
+//           display: false,
+//           tickMarkLength: 15,
+//         },
+//         ticks: {
+//           fontColor: "#9aa0ac", // Font Color
+//         }
+//       }]
+//     },
+//   }
+// });
 
 var ctx = document.getElementById("myChart2").getContext('2d');
 var myChart = new Chart(ctx, {
