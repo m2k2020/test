c5964216d66a8046983cd8df47f7c6e597cde636
musharaf Faqi <maamaanka55@gmail.com>
2023-04-27 12:46:55 +0300

    Success

--- testfileupload/app/views.py
+++ testfileupload/app/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, redirect
+from .forms import AuthorForm
+from django.core.files.storage import FileSystemStorage
+from .models import Author
 
 # Create your views here.
+
+
+
+def author(request):
+    # if request.method == 'POST':
+    #     form = AuthorForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         print("Save Author")
+    # else:
+    # form = AuthorForm()
+
+    if request.method == 'POST':
+        # name = request.POST.get('name')
+        filename = request.FILES.get('file')
+        # print(name)
+        print(filename)
+
+
+        fs = FileSystemStorage()
+        fname = fs.save(filename.name, filename)
+        fapath = fs.path(fname)
+
+        # print(fname)
+        # print(fapath)
+        # s=""
+
+        files = open(fapath,"r")
+        data = json.load(files)
+
+        if data:
+            for item in data:
+                name = item['name']
+                age = item['age']
+                adding = Author(name=name,age=age)
+                adding.save()
+                # print(name,age)
+            # Access the keys and values of each dictionary
+                # for key, val in item.items():
+                #     print(key ,val,"\n")
+
+        else:
+            print("No Captured data")
+
+    return render(request, 'author.html')
