9e11c9e7509e4242516d966c42ffdb38eb2db1d0
musharaf Faqi <maamaanka55@gmail.com>
2023-04-27 12:46:43 +0300

    Success

--- testfileupload/app/models.py
+++ testfileupload/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(null=True)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
