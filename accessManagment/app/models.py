cab7114920e4fcd096f6b235d6ff16911b12c5c7
musharaf Faqi <maamaanka55@gmail.com>
2023-05-03 08:59:25 +0300

    Success

--- accessManagment/app/models.py
+++ accessManagment/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cases(models.Model):
+    casetype = models.CharField(max_length=10)
+    crimename = models.CharField(max_length=50)
+
+class Files(models.Model):
+    file = models.FileField(upload_to='files/')
+    date = models.DateField(auto_now_add=True)
+    cases = models.ForeignKey(Cases, on_delete=models.CASCADE)
